54b36a9d3db6a67d5255cec2369c0fba1abe7ffa
Michael Auteri <mauteri@gmail.com>
2020-04-27 16:02:24 -0400

    Small adjustment.

--- sidebar.php
+++ sidebar.php
@@ -14,7 +14,11 @@ if ( ! is_active_sidebar( 'sidebar-1' ) && 'gp_event' !== get_post_type() ) {
 
 <aside id="secondary" class="sidebar-area">
 	<div class="wrap">
-		<?php get_template_part( 'template-parts/sidebar', get_post_type() ); ?>
+		<?php
+		if ( is_single() ) {
+			get_template_part( 'template-parts/sidebar', get_post_type() );
+		}
+		?>
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 </aside><!-- #secondary -->
